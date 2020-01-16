5b7132ab5ed5a886c5ff8a202bba7048eb16c1b3
James <mail7788897887@gmail.com>
2020-01-16 02:01:28 +0000

    sneks.

--- play.js
+++ play.js
@@ -2,3 +2,28 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  handleUserInput(stdin);
+
+  return stdin;
+}
+  
+  //setupInput();
+
+
+ const handleUserInput = function(stdin) {
+   stdin.on('data', (key) => { 
+     if (key = '\0003') {
+       process.stdout.write("Thanksssss.");
+       process.exit();
+     }
+   })
+ };
+
+
