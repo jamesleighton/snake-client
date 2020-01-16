71229c260104e52974cf7ae89261b2265ce7edf0
James <mail7788897887@gmail.com>
2020-01-16 02:10:27 +0000

    play refractored

--- play.js
+++ play.js
@@ -1,29 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  handleUserInput(stdin);
-
-  return stdin;
-}
-  
-  //setupInput();
-
-
- const handleUserInput = function(stdin) {
-   stdin.on('data', (key) => { 
-     if (key = '\0003') {
-       process.stdout.write("Thanksssss.");
-       process.exit();
-     }
-   })
- };
+setupInput()
 
 
